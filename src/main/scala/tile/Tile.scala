36d8165a3125d052bb9ce4c25b3d0789538370b2
monikerzju <3170105371@zju.edu.cn>
2021-03-23 14:11:45 +0800

    [tile] add some basic topology info in Tile.scala, successfully generate Tile.v with sbt command

---
+++ src/main/scala/tile/Tile.scala
@@ -0,0 +1,34 @@
+package tile
+
+import chisel3._
+import chisel3.stage._
+
+// tile(Yuan) = core + cache + uncache
+// core = datapath(Shen) + control(Shen) + coprocessor(Yuan)
+// cache = icache(Shi) + dcache(Shi)
+
+// AXI3 Protocol according to Loongson
+// Interrupt(s)
+class TileIO extends Bundle {
+
+}
+
+class Tile extends Module {
+  val io = IO(new TileIO)
+
+//   val core = Module(new Core)
+//   val icache = Module(new ICache)
+//   val dcache = Module(new DCache)
+//   val xbar = Module(new XBar)
+}
+
+// run `sbt "runMain tile.Main"` in terminal
+object Main {
+  def main(args: Array[String]): Unit = {
+    val packageName = this.getClass.getPackage.getName
+
+    (new chisel3.stage.ChiselStage).execute(
+      Array("-td", "build/verilog/"+packageName, "-X", "verilog"),
+      Seq(ChiselGeneratorAnnotation(() => new Tile)))
+  }
+}
