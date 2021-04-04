8c55f80ec4ef25436c2a30be8777d95749886af5
monikerzju <3170105371@zju.edu.cn>
2021-04-04 11:14:49 +0800

    [regfile] add multiple rw ports, deal with waw

--- src/main/scala/icore/RegFile.scala
+++ src/main/scala/icore/RegFile.scala
@@ -4,23 +4,25 @@ import chisel3._
 import chisel3.util._
 import chisel3.util.experimental._
 
-class RegFileIO(nregs: Int = 32, len: Int = 32) extends Bundle {
-  val rs1_addr = Input(UInt(log2Ceil(nregs).W))
-  val rs1_data = Output(UInt(len.W))
-  val rs2_addr = Input(UInt(log2Ceil(nregs).W))
-  val rs2_data = Output(UInt(len.W))
-  val wen      = Input(Bool())
-  val rd_addr  = Input(UInt(log2Ceil(nregs).W))
-  val rd_data  = Input(UInt(len.W))
+// issue order -> vec(0), vec(1), vec(2) ... ->> deal with WAW
+class RegFileIO(nregs: Int = 32, len: Int = 32, nread: Int = 6, nwrite: Int = 3) extends Bundle {
+  val rs_addr_vec = Input(Vec(nread, UInt(log2Ceil(nregs).W)))
+  val rs_data_vec = Output(Vec(nread, UInt(len.W)))
+  val wen_vec     = Input(Vec(nwrite, Bool()))
+  val rd_addr_vec = Input(Vec(nwrite, UInt(log2Ceil(nregs).W)))
+  val rd_data_vec = Input(Vec(nwrite, UInt(len.W)))
 }
 
-class RegFile(nregs: Int = 32, len: Int = 32) extends Module {
-  val io = IO(new RegFileIO(nregs, len))
+class RegFile(nregs: Int = 32, len: Int = 32, nread: Int = 6, nwrite: Int = 3) extends Module {
+  val io = IO(new RegFileIO(nregs, len, nread, nwrite))
 
   val regs = RegInit(VecInit(Seq.fill(nregs)(0.U(len.W))))
-  io.rs1_data := Mux(io.rs1_addr.orR, regs(io.rs1_addr), 0.U)
-  io.rs2_data := Mux(io.rs2_addr.orR, regs(io.rs2_addr), 0.U)
-  when(io.wen & io.rd_addr.orR) {
-    regs(io.rd_addr) := io.rd_data
+  for (i <- 0 until nread) {
+    io.rs_data_vec(i) := regs(io.rs_addr_vec(i))
+  }
+  for (i <- 0 until nwrite) {
+    when(io.wen_vec(i) && io.rd_addr_vec(i).orR) {
+      regs(io.rd_addr_vec(i)) := io.rd_data_vec(i)
+    }
   }
 }
