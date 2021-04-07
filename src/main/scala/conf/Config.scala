053620d599983f058499eda563bea533e1f05c21
monikerzju <3170105371@zju.edu.cn>
2021-04-07 19:57:04 +0800

    [bpu config bram] add 2-port bpu

--- src/main/scala/conf/Config.scala
+++ src/main/scala/conf/Config.scala
@@ -3,8 +3,13 @@ package conf
 import chisel3._
 
 trait Config {
-  val len: Int = 32
-  val startAddr: String = "hbfc00000"
-  val frontendIssueN: Int = 2   // 1 or 2
-  val withRealCache: Boolean = false
+  var len: Int = 32
+  var startAddr: String = "hbfc00000"
+  var frontendIssueN: Int = 2   // 1 or 2
+  // Cache
+  var withRealCache: Boolean = false
+  // BPU
+  var withBPU: Boolean = false 
+  var BPUEntryN: Int = 512
+  var BPUOffset: Int = 2
 }
