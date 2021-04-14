239f6341ebe765649f68e0c5266ce2b7320fc246
monikerzju <3170105371@zju.edu.cn>
2021-04-14 17:36:22 +0800

    [bram] add single and tru dual port ram

--- src/main/scala/cache/BRAM.scala
+++ src/main/scala/cache/BRAM.scala
@@ -3,7 +3,10 @@ package cache
 import chisel3._
 import chisel3.util._
 
-class BRAMWrapperIO(width: Int = 128, depth: Int = 16) extends Bundle {
+// There are 2 types of RAM, true dual port ram and single port ram
+
+// Dual Port BRAM
+class DPBRAMWrapperIO(width: Int = 128, depth: Int = 16) extends Bundle {
   val clk = Input(Clock())
   val rst = Input(Reset())
   val wea = Input(Bool())
@@ -20,11 +23,94 @@ class BRAMWrapperIO(width: Int = 128, depth: Int = 16) extends Bundle {
 
 class dual_port_ram(DATA_WIDTH: Int, DEPTH: Int, LATENCY: Int = 1) extends BlackBox(Map("DATA_WIDTH" -> DATA_WIDTH,
                                                                                         "DEPTH" -> DEPTH,
-                                                                                        "LATENCY" -> LATENCY)) {
-  val io = IO(new BRAMWrapperIO(DATA_WIDTH, DEPTH))
+                                                                                        "LATENCY" -> LATENCY)) with HasBlackBoxInline {
+  val io = IO(new DPBRAMWrapperIO(DATA_WIDTH, DEPTH))
+
+  setInline("dual_port_ram.v",
+  s"""
+    module dual_port_ram #(
+    	parameter DATA_WIDTH = 32,
+    	parameter DEPTH      = 1024,
+    	parameter LATENCY    = 1,
+    	parameter LATENCY_A  = LATENCY,
+    	parameter LATENCY_B  = LATENCY
+    ) (
+    	input  clk,
+    	input  rst,
+    	input  wea,
+    	input  web,
+    	input  ena,
+    	input  enb,
+    	input  [$$clog2(DEPTH)-1:0] addra,
+    	input  [$$clog2(DEPTH)-1:0] addrb,
+    	input  [DATA_WIDTH-1:0] dina,
+    	input  [DATA_WIDTH-1:0] dinb,
+    	output [DATA_WIDTH-1:0] douta,
+    	output [DATA_WIDTH-1:0] doutb
+    );
+
+    // xpm_memory_tdpram: True Dual Port RAM
+    // Xilinx Parameterized Macro, Version 2016.2
+    xpm_memory_tdpram #(
+    	// Common module parameters
+    	.MEMORY_SIZE(DATA_WIDTH * DEPTH),
+    	.MEMORY_PRIMITIVE("auto"),
+    	.CLOCKING_MODE("common_clock"),
+    	.USE_MEM_INIT(0),
+    	.WAKEUP_TIME("disable_sleep"),
+    	.MESSAGE_CONTROL(0),
+
+    	// Port A module parameters
+    	.WRITE_DATA_WIDTH_A(DATA_WIDTH),
+    	.READ_DATA_WIDTH_A(DATA_WIDTH),
+    	.READ_RESET_VALUE_A("0"),
+    	.READ_LATENCY_A(LATENCY_A),
+    	.WRITE_MODE_A("read_first"),
+
+    	// Port B module parameters
+    	.WRITE_DATA_WIDTH_B(DATA_WIDTH),
+    	.READ_DATA_WIDTH_B(DATA_WIDTH),
+    	.READ_RESET_VALUE_B("0"),
+    	.READ_LATENCY_B(LATENCY_B),
+    	.WRITE_MODE_B("read_first")
+    ) xpm_mem (
+    	// Common module ports
+    	.sleep          ( 1'b0  ),
+
+    	// Port A module ports
+    	.clka           ( clk   ),
+    	.rsta           ( rst   ),
+    	.ena            ( ena   ),
+    	.regcea         ( 1'b0  ),
+    	.wea            ( wea   ),
+    	.addra          ( addra ),
+    	.dina           ( dina  ),
+    	.injectsbiterra ( 1'b0  ), // do not change
+    	.injectdbiterra ( 1'b0  ), // do not change
+    	.douta          ( douta ),
+    	.sbiterra       (       ), // do not change
+    	.dbiterra       (       ), // do not change
+
+    	// Port B module ports
+    	.clkb           ( clk   ),
+    	.rstb           ( rst   ),
+    	.enb            ( enb   ),
+    	.regceb         ( 1'b0  ),
+    	.web            ( web   ),
+    	.addrb          ( addrb ),
+    	.dinb           ( dinb  ),
+    	.injectsbiterrb ( 1'b0  ), // do not change
+    	.injectdbiterrb ( 1'b0  ), // do not change
+    	.doutb          ( doutb ),
+    	.sbiterrb       (       ), // do not change
+    	.dbiterrb       (       )  // do not change
+    );
+
+    endmodule
+   """.stripMargin)
 }
 
-class BRAMSyncReadMemIO(DATA_WIDTH: Int, DEPTH: Int) extends Bundle {
+class DPBRAMSyncReadMemIO(DATA_WIDTH: Int, DEPTH: Int) extends Bundle {
   val wea   = Input(Bool())
   val web   = Input(Bool())
   val addra = Input(UInt(log2Ceil(DEPTH).W))
@@ -35,9 +121,9 @@ class BRAMSyncReadMemIO(DATA_WIDTH: Int, DEPTH: Int) extends Bundle {
   val doutb = Output(UInt(DATA_WIDTH.W))
 }
 
-class BRAMSyncReadMem(DEPTH: Int, DATA_WIDTH: Int, LATENCY: Int = 1) extends Module {
+class DPBRAMSyncReadMem(DEPTH: Int, DATA_WIDTH: Int, LATENCY: Int = 1) extends Module {
   val dpr = Module(new dual_port_ram(DATA_WIDTH, DEPTH, LATENCY))
-  val io = IO(new BRAMSyncReadMemIO(DATA_WIDTH, DEPTH))
+  val io = IO(new DPBRAMSyncReadMemIO(DATA_WIDTH, DEPTH))
 
   dpr.io.clk    := clock
   dpr.io.rst    := reset
@@ -51,4 +137,92 @@ class BRAMSyncReadMem(DEPTH: Int, DATA_WIDTH: Int, LATENCY: Int = 1) extends Mod
   dpr.io.dinb   := io.dinb
   io.douta      := dpr.io.douta
   io.doutb      := dpr.io.doutb
+}
+
+// Single port
+class BRAMWrapperIO(width: Int = 128, depth: Int = 16) extends Bundle {
+  val clk = Input(Clock())
+  val rst = Input(Reset())
+  val we  = Input(Bool())
+  val addr = Input(UInt(log2Ceil(depth).W))
+  val din = Input(UInt(width.W))
+  val dout = Output(UInt(width.W))
+}
+
+class single_port_ram(DATA_WIDTH: Int, DEPTH: Int, LATENCY: Int = 1) extends BlackBox(Map("DATA_WIDTH" -> DATA_WIDTH,
+                                                                                        "DEPTH" -> DEPTH,
+                                                                                        "LATENCY" -> LATENCY)) with HasBlackBoxInline {
+  val io = IO(new BRAMWrapperIO(DATA_WIDTH, DEPTH))
+
+  setInline("single_port_ram.v",
+  s"""
+    module single_port_ram (
+      parameter DATA_WIDTH = 32,
+    	parameter DEPTH      = 1024,
+    	parameter LATENCY    = 1,
+    )(
+    	input  clk,
+    	input  rst,
+    	input  we,
+    	input  [$$clog2(DEPTH)-1:0] addr,
+    	input  [DATA_WIDTH-1:0]  din,
+    	output [DATA_WIDTH-1:0]  dout
+    );
+
+    // xpm_memory_spram: Single Port RAM
+    // Xilinx Parameterized Macro, Version 2016.2
+    xpm_memory_spram #(
+    	// Common module parameters
+    	.MEMORY_SIZE(DATA_WIDTH * DEPTH),
+    	.MEMORY_PRIMITIVE("auto"),
+    	.USE_MEM_INIT(0),
+    	.WAKEUP_TIME("disable_sleep"),
+    	.MESSAGE_CONTROL(0),
+
+    	// Port A module parameters
+    	.WRITE_DATA_WIDTH_A(DATA_WIDTH),
+    	.READ_DATA_WIDTH_A(DATA_WIDTH),
+    	.READ_RESET_VALUE_A("0"),
+    	.READ_LATENCY_A(LATENCY),
+    	.WRITE_MODE_A("write_first")
+    ) xpm_mem (
+    	// Common module ports
+    	.sleep          ( 1'b0  ),
+
+    	// Port A module ports
+    	.clka           ( clk   ),
+    	.rsta           ( rst   ),
+    	.ena            ( 1'b1  ),
+    	.regcea         ( 1'b0  ),
+    	.wea            ( we    ),
+    	.addra          ( addr  ),
+    	.dina           ( din   ),
+    	.injectsbiterra ( 1'b0  ), // do not change
+    	.injectdbiterra ( 1'b0  ), // do not change
+    	.douta          ( dout  ),
+    	.sbiterra       (       ), // do not change
+    	.dbiterra       (       )  // do not change
+    );
+
+    endmodule
+   """.stripMargin)
+}
+
+class BRAMSyncReadMemIO(DATA_WIDTH: Int, DEPTH: Int) extends Bundle {
+  val we    = Input(Bool())
+  val addr  = Input(UInt(log2Ceil(DEPTH).W))
+  val din   = Input(UInt(DATA_WIDTH.W))
+  val dout  = Output(UInt(DATA_WIDTH.W))
+}
+
+class BRAMSyncReadMem(DEPTH: Int, DATA_WIDTH: Int, LATENCY: Int = 1) extends Module {
+  val spr = Module(new single_port_ram(DATA_WIDTH, DEPTH, LATENCY))
+  val io = IO(new BRAMSyncReadMemIO(DATA_WIDTH, DEPTH))
+
+  spr.io.clk    := clock
+  spr.io.rst    := reset
+  spr.io.we     := io.we
+  spr.io.addr   := io.addr
+  spr.io.din    := io.din
+  io.dout       := spr.io.dout
 }
