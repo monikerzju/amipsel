a6c30dd70379a357e7b4b07460afc74100a4d0a6
Shay P.C <3180102686@zju.edu.cn>
2021-04-15 17:14:44 +0800

    [ignore] solve merge conflict

--- src/test/scala/icache_test.scala
+++ src/test/scala/icache_test.scala
@@ -20,8 +20,8 @@ class cache_test extends FreeSpec with ChiselScalatestTester with Cache_Paramete
             c.io.AXI.req.setSinkClock(c.clock)
             c.io.cpu.resp.initSink()
             c.io.cpu.resp.setSinkClock(c.clock)
-
-            val input_values=Seq.tabulate(10){i => (i<<6).U(32.W)}
+            val k=10
+            val input_values=Seq.tabulate(k){i => (i<<6).U(32.W)}
             val cpu_request_vector=input_values.map{i => 
                 new MemReq().Lit(_.addr->i,_.wdata->0.U,_.wen->false.B,_.flush->false.B,_.invalidate->false.B,_.mtype->0.U)
             }
@@ -29,41 +29,41 @@ class cache_test extends FreeSpec with ChiselScalatestTester with Cache_Paramete
                 new AXI4IO_Req().Lit(_.addr->i)
             }
             
-            val AXI_response_vector=Seq.tabulate(10){i => 
+            val AXI_response_vector=Seq.tabulate(k){i => 
                 new AXIResp().Lit(
                     _.rdata->(1000+i).U(512.W)
                 )
             }
-            val cpu_response_vector=Seq.tabulate(10){i => 
+            val cpu_response_vector=Seq.tabulate(k){i => 
                 new MemResp().Lit(
                     _.rdata->(1000+i).U(32.W)
                 )
             }
             // println(cpu_request_vector)
-            // c.io.cpu.req.enqueueNow(cpu_request_vector(0))
+            c.io.cpu.req.enqueueNow(cpu_request_vector(4))
 
-            // c.io.AXI.req.expectDequeueNow(AXI_request_vector(0))
-            fork{
-                c.io.cpu.req.enqueueSeq(cpu_request_vector)     
-            }
-            .fork{
-                fork{
-                    c.io.AXI.req.expectDequeueSeq(AXI_request_vector)     
-                }
-                .fork{
-                    c.io.AXI.resp.enqueueSeq(AXI_response_vector)  
-                }
-                .join()
-            }
-            .fork{
-                c.io.cpu.resp.expectDequeueSeq(cpu_response_vector)     
-            }.join()
-            
+            c.io.AXI.req.expectDequeueNow(AXI_request_vector(4))
             // fork{
             //     c.io.cpu.req.enqueueSeq(cpu_request_vector)     
-            // }.fork{
-            //     c.io.cpu.resp.expectDequeueSeq(cpu_response_vector)
+            // }
+            // .fork{
+            //     fork{
+            //         c.io.AXI.req.expectDequeueSeq(AXI_request_vector)     
+            //     }
+            //     .fork{
+            //         c.io.AXI.resp.enqueueSeq(AXI_response_vector)  
+            //     }
+            //     .join()
+            // }
+            // .fork{
+            //     c.io.cpu.resp.expectDequeueSeq(cpu_response_vector)     
             // }.join()
+            
+            fork{
+                c.io.cpu.req.enqueueSeq(cpu_request_vector)     
+            }.fork{
+                c.io.cpu.resp.expectDequeueSeq(cpu_response_vector)
+            }.join()
         }
     }
 }
