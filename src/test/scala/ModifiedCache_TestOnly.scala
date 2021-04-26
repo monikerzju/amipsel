9dcd76121ca0ae56b7d27edbf5bf3765ee730c33
Shay P.C <3180102686@zju.edu.cn>
2021-04-26 08:58:13 +0800

    [mmio] stashed test for D$

--- src/test/scala/ModifiedCache_TestOnly.scala
+++ src/test/scala/ModifiedCache_TestOnly.scala
@@ -16,7 +16,10 @@ class DCacheSimple_test extends Module with CacheParameters with MemAccessType w
     // val data=Module(new DPBRAMSyncReadMem(nline,1<<(OffsetBits+3)))
     val meta=Module(new MetaDataSimple(nline));
 
-    val tag_raw=io.cpu.req.bits.addr(31,32-TagBits)
+    val unmaped=io.cpu.req.bits.addr(31,29)==="b100".U
+    // 0x80000000-0xa000000
+    // translate virtual addr from start
+    val tag_raw=Cat(Mux(unmaped,0.U(3.W),io.cpu.req.bits.addr(31,29)),io.cpu.req.bits.addr(28,32-TagBits))
     val index_raw=io.cpu.req.bits.addr(len-TagBits-1,len-TagBits-IndexBits)
     io.bar.req.valid:=false.B
     io.bar.req.wen:=false.B
@@ -44,7 +47,6 @@ class DCacheSimple_test extends Module with CacheParameters with MemAccessType w
     val writeline=Wire(Vec(1<<(OffsetBits-2),UInt(len.W)))
     writeline:=line
     val mask=Wire(UInt(32.W))
-    // val shift=index_raw(1,0)<<3
     val shift=io.cpu.req.bits.addr(1,0)<<3
     mask:="hffffffff".U
     switch(io.cpu.req.bits.mtype){
@@ -62,26 +64,27 @@ class DCacheSimple_test extends Module with CacheParameters with MemAccessType w
     val wd=((mask & wdata)<<shift) | ((~(mask << shift)) & line(word1))
 
     io.cpu.req.ready:=io.cpu.resp.valid
-    val reg_addr=RegNext(index_raw)
     io.cpu.resp.valid:=io.bar.resp.valid||meta.io.hit
     val dual_issue=io.cpu.req.bits.mtype===3.U && word2=/=0.U
     // io.cpu.resp.bits.respn:= Cat(dual_issue,!meta.io.hit)
     io.cpu.resp.bits.respn:= dual_issue
     io.cpu.resp.bits.rdata(0):=line(word1)
     io.cpu.resp.bits.rdata(1):=line(word2)
-    val s_normal::s_evict::s_refill::Nil=Enum(3)
+    val s_normal::s_evict::s_refill::s_uncached::Nil=Enum(4)
     val state=RegInit(s_normal)
     val tag_evict_reg=RegInit(0.U(TagBits.W))
     io.data.web:=reg_wen
     // FIXME: [ ] write miss?
-    io.data.addrb:=reg_addr
+    io.data.addrb:=index
     io.data.dinb:=writeline.asUInt
+    val mmio=io.cpu.req.bits.addr(31,29)==="b101".U // A000_0000-C000_0000
+    io.bar.req.mtype:=Mux(mmio,io.cpu.req.bits.mtype,MEM_DWORD.U)
     when(reg_wen){
         writeline(word1):=wd
     }
     switch(state){
         is(s_normal){
-            when(io.cpu.req.valid){
+            when(!mmio&&io.cpu.req.valid){
                 when(meta.io.hit){
                     // nothing to be done, data on the way
                 }
@@ -103,6 +106,14 @@ class DCacheSimple_test extends Module with CacheParameters with MemAccessType w
                     }
                 }
             }
+            .elsewhen(mmio && io.cpu.req.valid){
+                // send with raw data right away
+                state:=s_uncached
+                io.bar.req.valid:=true.B
+                io.bar.req.addr:=io.cpu.req.bits.addr
+                io.bar.req.data:=io.cpu.req.bits.wdata
+                io.bar.req.wen:=io.cpu.req.bits.wen
+            }
         }
         is(s_refill){
             io.bar.req.addr:=Cat(Seq(tag_refill,index_refill,0.U(OffsetBits.W)))
@@ -113,19 +124,34 @@ class DCacheSimple_test extends Module with CacheParameters with MemAccessType w
                 meta.io.update:=true.B
                 io.data.addra:=index_refill
                 io.data.wea:=true.B
+            }.otherwise {
+                io.bar.req.valid:=true.B
             }
         }
         is(s_evict){
             io.bar.req.valid:=true.B
             io.bar.req.wen:=true.B
             io.bar.req.data:=line.asUInt
-            // FIXME: register?
+            // FIXME: [ ] register for line?
             io.bar.req.addr:=Cat(Seq(tag_evict_reg,index_refill,0.U(OffsetBits.W)))
             when(io.bar.resp.valid){
                 state:=s_refill
-                io.bar.req.valid:=true.B
                 io.bar.req.addr:=Cat(Seq(tag_refill,index_refill,0.U(OffsetBits.W)))
                 io.bar.req.wen:=false.B
+            }
+        }
+        is(s_uncached){
+            io.bar.req.valid:=true.B
+            io.bar.req.addr:=io.cpu.req.bits.addr
+            io.bar.req.data:=io.cpu.req.bits.wdata
+            io.bar.req.wen:=io.cpu.req.bits.wen
+
+            when(io.bar.resp.valid){
+                state:=s_normal
+                when(!reg_wen){
+                    io.cpu.resp.bits.rdata(0):=io.bar.resp.data(31,0)
+                }
+                // nothing to be done for write uncached 
             }
         }
     }
