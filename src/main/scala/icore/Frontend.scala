a1318462a752e3672a55771466cffbc3642ef489
monikerzju <3170105371@zju.edu.cn>
2021-04-30 01:20:11 +0800

    [backend] rewrite to fit in 1 latency delay icache data and meta

--- src/main/scala/icore/Frontend.scala
+++ src/main/scala/icore/Frontend.scala
@@ -24,66 +24,65 @@ class PCGen(va_width: Int = 32, start_va: String = "h80000000", increment: Int =
 class Frontend extends Module with Config with MemAccessType with FrontToBack {
   val io = IO(new FrontendIO)
 
-  val pc_gen = Module(new PCGen(len, startAddr, 4 * frontendIssueN))
-  val decs = Array.fill(frontendIssueN)(Module(new Dec).io)
-  val decode_pc_low = RegInit(UInt(len.W), startAddr.U)
-  val decode_instn = RegInit(UInt(frontendIssueN.W), 0.U)
-  val decode_reg_line = Array.fill(frontendIssueN)(RegInit(0.U(len.W)))
-  val last_wait = RegNext(pc_gen.io.please_wait)
-
-  // Signals define
+  // IF
+  val pc_gen         = Module(new PCGen(len, startAddr, 4 * frontendIssueN))
+  val cache_stall    = Wire(Bool())
+  val fetch_half     = Wire(Bool())
   val last_req_valid = RegInit(false.B)
+  val repc           = RegInit(startAddr.U(len.W))
+  val stall_f        = Wire(Bool())
+  val kill_f         = Wire(Bool())
+
+  // ID
+  val decode_pc_low_reg = RegInit(UInt(len.W), startAddr.U)
+  val decode_valid_reg  = RegInit(false.B)
+  val decs              = Array.fill(frontendIssueN)(Module(new Dec).io)
+  val stall_d           = Wire(Bool())
+  val kill_d            = Wire(Bool())
+
+  // IF Stage
+  stall_f        := stall_d || cache_stall
+  kill_f         := kill_d
+  fetch_half     := io.icache.resp.bits.respn === 0.U
+  cache_stall    := (if (metaZeroLatency) RegNext(!io.icache.resp.valid) else !io.icache.resp.valid) && last_req_valid
   last_req_valid := io.icache.req.valid
-  val icache_stall_req_a = (if (metaZeroLatency) RegNext(!io.icache.resp.valid) else !io.icache.resp.valid) && last_req_valid
-  val icache_fetch_half_a = !io.icache.resp.bits.respn    // Combo logic
-  val wtg = last_wait
-  val gtw = !last_wait && pc_gen.io.please_wait
 
-  // Replay PC
-  val repc = RegInit(startAddr.U(len.W))
-  repc := Mux(icache_stall_req_a, repc, pc_gen.io.pc_o)
+  pc_gen.io.please_wait := stall_f
+  pc_gen.io.redirect    := kill_f || (fetch_half && !cache_stall)
+  pc_gen.io.redirect_pc := Mux(kill_f, io.fb.bmfs.redirect_pc, pc_gen.io.pc_o + 4.U)
 
-  // [---------- IF Stage ----------]
-  // redirect_pc prio: bmfs.redirect > icache only fetch one
-  // Only please wait from icache is atomic and will cause bmfs.please_wait = 1
-  val wait_icache = icache_stall_req_a
-  pc_gen.io.please_wait := wait_icache || io.fb.fmbs.please_wait
-  pc_gen.io.redirect := io.fb.bmfs.redirect_kill || (icache_fetch_half_a && !wait_icache)  // TODO when BPU is added
-  pc_gen.io.redirect_pc := Mux(io.fb.bmfs.redirect_kill, io.fb.bmfs.redirect_pc, pc_gen.io.pc_o + 4.U)  // TODO
+  repc := Mux(stall_f, repc, pc_gen.io.pc_o)
 
-  io.icache.req.valid := true.B // maybe situation will change by adding BPU
-  io.icache.resp.ready := true.B 
-  io.icache.req.bits.addr := Mux(icache_stall_req_a, repc, pc_gen.io.pc_o)
+  io.icache.req.valid      := true.B
+  io.icache.resp.ready     := true.B 
+  io.icache.req.bits.addr  := Mux(stall_f, repc, pc_gen.io.pc_o)
   io.icache.req.bits.wdata := DontCare
-  io.icache.req.bits.wen := false.B
+  io.icache.req.bits.wen   := false.B
   if (frontendIssueN == 1) {
-    io.icache.req.bits.mtype :=  MEM_WORD.U
+    io.icache.req.bits.mtype := MEM_WORD.U
   } else {
-    io.icache.req.bits.mtype :=  MEM_DWORD.U
+    io.icache.req.bits.mtype := MEM_DWORD.U
   }
   io.icache.req.bits.flush := false.B
   io.icache.req.bits.invalidate := false.B
 
-  // [---------- ID Stage ----------]
-  // for 1 cycle latency
-  for (i <- 0 until frontendIssueN) {
-    decode_reg_line(i) := Mux(gtw, io.icache.resp.bits.rdata(i), decode_reg_line(i))
+  // ID Stage
+  stall_d := io.fb.fmbs.please_wait
+  kill_d  := io.fb.bmfs.redirect_kill
+
+  when (kill_d) {
+    decode_valid_reg  := false.B
+  }.elsewhen (!stall_d && !cache_stall) {
+    decode_pc_low_reg := io.icache.req.bits.addr
+    decode_valid_reg  := io.icache.req.valid
   }
-  // for 0 cycle latency
-  val delayed_redirect_kill = RegInit(false.B)
-  delayed_redirect_kill := Mux(!icache_stall_req_a, false.B, 
-    Mux(io.fb.bmfs.redirect_kill || RegNext(io.fb.bmfs.redirect_kill), true.B, delayed_redirect_kill)
+
+  io.fb.fmbs.instn := Mux(stall_d, 0.U,
+    Mux(!cache_stall && decode_valid_reg, Cat(0.U, RegNext(io.icache.resp.bits.respn)) + 1.U, 0.U)
   )
-  decode_pc_low := Mux(io.fb.fmbs.please_wait, decode_pc_low, io.icache.req.bits.addr)
-  decode_instn := Mux(
-    io.fb.bmfs.redirect_kill, 0.U, 
-    Mux(io.fb.fmbs.please_wait, decode_instn, Cat(0.U, io.icache.resp.bits.respn) + 1.U)
-  )
-  // some IO to the fifo backend
-  io.fb.fmbs.instn := Mux(io.fb.fmbs.please_wait, 0.U, Mux(!wait_icache && !delayed_redirect_kill, decode_instn, 0.U))
   for (i <- 0 until frontendIssueN) {
-    decs(i).inst := Mux(icache_stall_req_a, decode_reg_line(i), io.icache.resp.bits.rdata(i))
-    decs(i).pc := decode_pc_low + (i.U << 2.U)
+    decs(i).inst           := io.icache.resp.bits.rdata(i)
+    decs(i).pc             := decode_pc_low_reg + (i.U << 2.U)
     io.fb.fmbs.inst_ops(i) := decs(i).mops.asUInt
   }
 
