af4f1920bab23a1989d9ea529adf39618ab3bb2d
monikerzju <3170105371@zju.edu.cn>
2021-07-19 12:55:44 +0800

    make tilevery for xingjian's verilator test

--- Makefile
+++ Makefile
@@ -18,3 +18,16 @@ tile:
 
 core:
 	sbt "runMain tile.GenC"
+
+tilevery:
+	sbt "runMain tile.GenTV"
+	cat $(DIR)/build/verilog/tile/single_port_ram.v >> build/verilog/tile/TileForVerilator.v
+	cat $(DIR)/build/verilog/tile/dual_port_ram.v >> build/verilog/tile/TileForVerilator.v
+	rm $(DIR)/build/verilog/tile/firrtl*
+	rm $(DIR)/build/verilog/tile/single_port_ram.v
+	rm $(DIR)/build/verilog/tile/dual_port_ram.v
+	rm $(DIR)/build/verilog/tile/TileForVerilator.fir
+	rm $(DIR)/build/verilog/tile/TileForVerilator.anno.json
+
+clean:
+	rm -rf build/
--- src/main/scala/tile/Tile.scala
+++ src/main/scala/tile/Tile.scala
@@ -54,6 +54,104 @@ class Tile(diffTestV: Boolean) extends Module with Config {
   io.axi3 <> xbar.io.axi3
 }
 
+// Verilator Shell, only for Loooooongson CUP, not configurable for simple
+class TileForVerilator extends RawModule {
+  val aclk         = IO(Input(Clock()))
+  val aresetn      = IO(Input(Bool()))
+  val ext_int      = IO(Input(UInt(6.W)))
+  // AXI
+  val arid         = IO(Output(UInt(4.W)))
+  val araddr       = IO(Output(UInt(32.W)))
+  val arlen        = IO(Output(UInt(8.W)))
+  val arsize       = IO(Output(UInt(3.W)))
+  val arburst      = IO(Output(UInt(2.W)))
+  val arlock       = IO(Output(UInt(2.W)))
+  val arcache      = IO(Output(UInt(4.W)))
+  val arprot       = IO(Output(UInt(3.W)))
+  val arvalid      = IO(Output(Bool()))
+  val arready      = IO(Input(Bool()))
+  val rid          = IO(Input(UInt(4.W)))
+  val rdata        = IO(Input(UInt(32.W)))
+  val rresp        = IO(Input(UInt(2.W)))
+  val rlast        = IO(Input(Bool()))
+  val rvalid       = IO(Input(Bool()))
+  val rready       = IO(Output(Bool()))
+  val awid         = IO(Output(UInt(4.W)))
+  val awaddr       = IO(Output(UInt(32.W)))
+  val awlen        = IO(Output(UInt(8.W)))
+  val awsize       = IO(Output(UInt(3.W)))
+  val awburst      = IO(Output(UInt(2.W)))
+  val awlock       = IO(Output(UInt(2.W)))
+  val awcache      = IO(Output(UInt(4.W)))
+  val awprot       = IO(Output(UInt(3.W)))
+  val awvalid      = IO(Output(Bool()))
+  val awready      = IO(Input(Bool()))
+  val wid          = IO(Output(UInt(4.W)))
+  val wdata        = IO(Output(UInt(32.W)))
+  val wstrb        = IO(Output(UInt(4.W)))
+  val wlast        = IO(Output(Bool()))
+  val wvalid       = IO(Output(Bool()))
+  val wready       = IO(Input(Bool()))
+  val bid          = IO(Input(UInt(4.W)))
+  val bresp        = IO(Input(UInt(2.W)))
+  val bvalid       = IO(Input(Bool()))
+  val bready       = IO(Output(Bool()))
+  // DBG
+  // val debug_wb_pc       = IO(Output(UInt(32.W)))
+  // val debug_wb_rf_wen   = IO(Output(UInt(4.W)))
+  // val debug_wb_rf_wnum  = IO(Output(UInt(5.W)))
+  // val debug_wb_rf_wdata = IO(Output(UInt(32.W)))
+
+  withClockAndReset(aclk, !aresetn) {
+    val tile = Module(new Tile(false))
+
+    arid      <>    tile.io.axi3.ar.bits.id 
+    araddr    <>    tile.io.axi3.ar.bits.addr   
+    arlen     <>    tile.io.axi3.ar.bits.len  
+    arsize    <>    tile.io.axi3.ar.bits.size   
+    arburst   <>    tile.io.axi3.ar.bits.burst    
+    arlock    <>    tile.io.axi3.ar.bits.lock   
+    arcache   <>    tile.io.axi3.ar.bits.cache    
+    arprot    <>    tile.io.axi3.ar.bits.prot   
+    arvalid   <>    tile.io.axi3.ar.valid    
+    arready   <>    tile.io.axi3.ar.ready   
+
+    rid       <>    tile.io.axi3.r.bits.id
+    rdata     <>    tile.io.axi3.r.bits.data  
+    rresp     <>    tile.io.axi3.r.bits.resp  
+    rlast     <>    tile.io.axi3.r.bits.last  
+    rvalid    <>    tile.io.axi3.r.valid   
+    rready    <>    tile.io.axi3.r.ready 
+
+    awid      <>    tile.io.axi3.aw.bits.id 
+    awaddr    <>    tile.io.axi3.aw.bits.addr   
+    awlen     <>    tile.io.axi3.aw.bits.len  
+    awsize    <>    tile.io.axi3.aw.bits.size   
+    awburst   <>    tile.io.axi3.aw.bits.burst    
+    awlock    <>    tile.io.axi3.aw.bits.lock   
+    awcache   <>    tile.io.axi3.aw.bits.cache    
+    awprot    <>    tile.io.axi3.aw.bits.prot   
+    awvalid   <>    tile.io.axi3.aw.valid    
+    awready   <>    tile.io.axi3.aw.ready    
+
+    wid       <>    tile.io.axi3.w.bits.id
+    wdata     <>    tile.io.axi3.w.bits.data  
+    wstrb     <>    tile.io.axi3.w.bits.strb  
+    wlast     <>    tile.io.axi3.w.bits.last  
+    wvalid    <>    tile.io.axi3.w.valid   
+    wready    <>    tile.io.axi3.w.ready 
+
+    bid       <>    tile.io.axi3.b.bits.id
+    bresp     <>    tile.io.axi3.b.bits.resp  
+    bvalid    <>    tile.io.axi3.b.valid   
+    bready    <>    tile.io.axi3.b.ready   
+
+    tile.io.intr := ext_int.asBools
+
+
+  }
+}
+
 // run `sbt "runMain tile.GenT"` in terminal
 object GenT {
   def main(args: Array[String]): Unit = {
@@ -72,5 +170,15 @@ object GenC {
     (new chisel3.stage.ChiselStage).execute(
       Array("-td", "build/verilog/"+packageName, "-X", "verilog"),
       Seq(ChiselGeneratorAnnotation(() => new Core(args.contains("-diff")))))
+  }
+}
+
+object GenTV {
+  def main(args: Array[String]): Unit = {
+    val packageName = this.getClass.getPackage.getName
+
+    (new chisel3.stage.ChiselStage).execute(
+      Array("-td", "build/verilog/"+packageName, "-X", "verilog"),
+      Seq(ChiselGeneratorAnnotation(() => new TileForVerilator)))
   }
 }
