67f260deab47e7dabdeda2c4059fb8a1442255b4
fishcrap <1063231619@qq.com>
2021-08-07 22:19:54 +0800

    [TLB] TLB initial, TODO: TLB exception

--- src/main/scala/conf/Config.scala
+++ src/main/scala/conf/Config.scala
@@ -26,6 +26,10 @@ trait Config {
   var dIndexBits: Int = 9   // 16KB now
   var offsetBits: Int = 5
   var dataBits: Int = 256
+  // TLB
+  var VPNSize: Int = 19
+  var PFNSize: Int = 20
+  var TLBSize: Int = 32
   // BPU
   var traceCallRet: Boolean = false
   var traceBPU: Boolean = false
